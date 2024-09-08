066ac69f5ed65e904add7fa2c5940a4f9ea4d76d
Thiago Andrade <thiagoandrade160302@gmail.com>
2024-09-08 14:46:53 -0300

    Problem two sum

---
+++ solutions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using leetcode.Quests;
+namespace leetcode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var resultTwoSums = TwoSum.Calculate([2,5,5,11],10);
+        foreach (var result in resultTwoSums)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
+
